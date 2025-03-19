666ee3321c726a0118b695068d15d067226f4e26
abhi1994j <abhibaban4@gmail.com>
2025-03-19 09:30:47 +0530

    complete ui

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const ctx = document.getElementById('myChart').getContext('2d');
-
+const data = {
+  labels: ['January' , 'February' , 'MArch' , 'April' , 'May' ],
+  datasets: [{
+    label: 'Monthly Sales',
+    data: [65,59,80,81,57],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.6)',  
+    ],
+    borderColor: [
+      'rgba(255, 99, 132, 1)',
+    ],
+    borderWidth: 1
+  }]
+}
 const myChart = new Chart(ctx, {
   type: 'bar',
-  data: {
-    labels: ['January' , 'February' , 'MArch' , 'April' , 'May' ],
-    datasets: [{
-      label: 'Monthly Sales',
-      data: [65,59,80,81,57],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.6)',  
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-      ],
-      borderWidth: 1
-    }]
-  },
+  data: data,
   options: {
     responsive: false,
     maintainAspectRatio: false,
