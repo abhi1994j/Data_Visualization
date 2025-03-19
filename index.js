5cbd1e7026b0ebe512500608279f789084624d1a
abhi1994j <abhibaban4@gmail.com>
2025-03-19 09:04:05 +0530

    complete ui

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const ctx = document.getElementById('myChart').getContext('2d');
 
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['January' , 'February' , 'MArch' , 'April' , 'May' ],
+    datasets: [{
+      label: 'Monthly Sales',
+      data: [65,59,80,81,57],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.6)',  
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: false,
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Monthly Sales Data',
+        font: {
+          size: 20
+        }
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return `Monthly Sales Data: ${context.raw}`;
+            // console.log(context);
+          }
+        }
+      }
+    }
+  }
+});
